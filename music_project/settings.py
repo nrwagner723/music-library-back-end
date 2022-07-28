07cb53dee95e3392a343e8a70021219325499164
Natalie Wagner <nrwagner723@gmail.com>
2022-07-28 13:36:18 -0500

    installed cors headers

--- music_project/settings.py
+++ music_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'music',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -46,7 +47,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'music_project.urls'
 
