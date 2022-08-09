6cb29211ef2791afbd50877650e8c2e499121fe7
Natalie Wagner <nrwagner723@gmail.com>
2022-08-09 13:59:27 -0500

    fixed an error with the models having the wrong field

--- music/models.py
+++ music/models.py
@@ -6,6 +6,5 @@ class Music(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    release_date = models.DateField()
+    release_date = models.CharField(max_length=255)
     genre = models.CharField(max_length=255)
-    likes = models.IntegerField()
